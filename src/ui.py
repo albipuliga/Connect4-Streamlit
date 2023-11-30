8e412bade02523bda84a662134ca5bfcced71874
Alberto Puliga <albi.puliga@gmail.com>
2023-11-30 17:10:25 +0100

    reset button not working

---
+++ src/ui.py
@@ -0,0 +1,105 @@
+import numpy as np
+import streamlit as st
+
+from utils import ConnectFour
+
+
+# Function to draw the board
+def draw(num_columns, num_rows):
+    st.title("Board")
+    columns = st.columns(num_columns)
+    acc_col = -1
+    acc_row = -1
+    while acc_col < 6:
+        for col in columns:
+            if acc_row > 4:
+                acc_row = -1
+
+            acc_col += 1
+            with col:
+                for circle in range(num_rows):
+                    acc_row += 1
+                    if st.session_state.game.board[acc_row][acc_col] == "X":  # player
+                        st.markdown(
+                            f'<div style="width: 50px; height: 50px; background-color: green; border-radius: 50%; margin: 10px;"></div>',
+                            unsafe_allow_html=True,
+                        )
+                    elif (
+                        st.session_state.game.board[acc_row][acc_col] == "O"
+                    ):  # machine
+                        st.markdown(
+                            f'<div style="width: 50px; height: 50px; background-color: red; border-radius: 50%; margin: 10px;"></div>',
+                            unsafe_allow_html=True,
+                        )
+                    else:
+                        st.markdown(
+                            f'<div style="width: 50px; height: 50px; background-color: grey; border-radius: 50%; margin: 10px;"></div>',
+                            unsafe_allow_html=True,
+                        )
+
+
+# Function for machine moves
+def machine_moves(game, player="O"):
+    col = game.greedy_move()
+    game.insert_disc(col, player)
+
+
+# Function to check if a column is full
+def is_col_full(board, col):
+    return board[0][col] != " "
+
+
+# Function to process player moves
+def player_moves(game, col, player="X"):
+    if not is_col_full(game.board, col):
+        game.insert_disc(col, player)
+
+
+st.title("Connect 4")
+num_columns = 7
+num_rows = 6
+
+
+# Initialize the game board and game_over flag
+if "game" not in st.session_state:
+    st.session_state.game = ConnectFour()
+    st.session_state.game_over = False
+
+# Choose who starts
+st.session_state.selected_option = st.selectbox("Who starts?", ["Machine", "User"])
+
+# Reset the game
+if st.button("Reset"):
+    st.session_state.game = ConnectFour()
+    if st.session_state.selected_option == "Machine":
+        machine_moves(st.session_state.game)
+
+# Player input
+col = st.number_input("Choose a column (0-6):", 0, num_columns - 1, format="%d")
+
+response_container = st.container()
+with response_container:
+    if st.button("Drop Chip"):
+        if st.session_state.game_over:
+            st.warning("Game over. Please reset the game to play again.")
+        elif is_col_full(st.session_state.game.board, col):
+            st.error("Column is full!")
+        else:
+            player_moves(st.session_state.game, col)
+            # Check if player won
+            if st.session_state.game.check_winner("X"):
+                draw(num_columns, num_rows)
+                st.success("You won!")
+                st.session_state.game_over = True
+                st.stop()
+
+            if not st.session_state.game_over:
+                machine_moves(st.session_state.game)
+                # Check if machine won
+                if st.session_state.game.check_winner("O"):
+                    draw(num_columns, num_rows)
+                    st.error("You lost!")
+                    st.session_state.game_over = True
+                    st.stop()
+
+    draw(num_columns, num_rows)
---
+++ src/utils.py
@@ -0,0 +1,74 @@
+class ConnectFour:
+    def __init__(self):
+        self.rows = 6
+        self.columns = 7
+        self.board = [[" " for _ in range(self.columns)] for _ in range(self.rows)]
+        self.turn = "X"  # X will start
+
+    def insert_disc(self, column, player):
+        if self.board[0][column] != " ":
+            return False  # Column is full
+
+        for row in range(self.rows - 1, -1, -1):
+            if self.board[row][column] == " ":
+                self.board[row][column] = player
+                return True
+        return False
+
+    def print_board(self):
+        for row in self.board:
+            print("|" + "|".join(row) + "|")
+        print("+".join(["---" for _ in range(self.columns)]))
+
+    def check_winner(self, player):
+        # Check horizontal, vertical, and diagonal for a win
+        for row in range(self.rows):
+            for col in range(self.columns - 3):
+                if all(self.board[row][c] == player for c in range(col, col + 4)):
+                    return True
+
+        for col in range(self.columns):
+            for row in range(self.rows - 3):
+                if all(self.board[r][col] == player for r in range(row, row + 4)):
+                    return True
+
+        for row in range(self.rows - 3):
+            for col in range(self.columns - 3):
+                if all(self.board[row + i][col + i] == player for i in range(4)):
+                    return True
+
+        for row in range(3, self.rows):
+            for col in range(self.columns - 3):
+                if all(self.board[row - i][col + i] == player for i in range(4)):
+                    return True
+
+        return False
+
+    def is_full(self):
+        return all(self.board[0][col] != " " for col in range(self.columns))
+
+    def greedy_move(self):
+        for col in range(self.columns):
+            if self.simulate_move(col, "O"):
+                return col
+
+        for col in range(self.columns):
+            if self.simulate_move(col, "X"):
+                return col
+
+        # Fallback strategy: choose the center or the closest available column
+        for offset in range(self.columns // 2 + 1):
+            for col in [self.columns // 2 + offset, self.columns // 2 - offset]:
+                if col >= 0 and col < self.columns and self.board[0][col] == " ":
+                    return col
+
+    def simulate_move(self, col, player):
+        for row in range(self.rows - 1, -1, -1):
+            if self.board[row][col] == " ":
+                self.board[row][col] = player
+                win = self.check_winner(player)
+                self.board[row][col] = " "  # Undo the move
+                if win:
+                    return True
+                break
+        return False
